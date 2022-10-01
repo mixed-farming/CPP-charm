99af8b3e49dbd9910e900f71326b6fc5b52acf5c
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-10-02 00:56:29 +0530

    Create addition_without'+'_and_bitwiseOp.cpp

---
+++ addition_without'+'_and_bitwiseOp.cpp
@@ -0,0 +1,48 @@
+// Addition without using "+" and Bitwise operators
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//try out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a,b;
+  cout <<"Enter the number a and b : ";
+  cin>>a>>b;
+  int c=a-(-b);
+  cout<<"Sum is "<<c;
+}
+
+// This might seem to be a silly question. But these kind of questions are asked in interviews to check your critical thinking :)
